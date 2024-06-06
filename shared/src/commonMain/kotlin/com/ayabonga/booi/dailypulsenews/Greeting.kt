0ab2aef730191a7e70a3518b4db7756873aa9ca6
Ayabonga <ayabongabooi2@gmail.com>
2024-06-06 20:46:36 +0200

    chore: remove Greeting.kt

--- shared/src/commonMain/kotlin/com/ayabonga/booi/dailypulsenews/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package com.ayabonga.booi.dailypulsenews
-
-class Greeting {
-    private val platform: Platform = getPlatform()
-
-    fun greeting(): String {
-        return "Hello, ${platform.name}!"
-    }
-}
