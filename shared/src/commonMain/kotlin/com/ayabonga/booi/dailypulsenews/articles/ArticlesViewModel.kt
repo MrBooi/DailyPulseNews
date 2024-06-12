37ae56197c1ba8070f3875439583678de089bc7a
Ayabonga <ayabongabooi2@gmail.com>
2024-06-12 22:30:34 +0200

    wip: add getArticles fun

--- shared/src/commonMain/kotlin/com/ayabonga/booi/dailypulsenews/articles/ArticlesViewModel.kt
+++ shared/src/commonMain/kotlin/com/ayabonga/booi/dailypulsenews/articles/ArticlesViewModel.kt
@@ -1,11 +1,24 @@
 package com.ayabonga.booi.dailypulsenews.articles
 
 import com.ayabonga.booi.dailypulsenews.BaseViewModel
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 
 class ArticlesViewModel : BaseViewModel() {
     private val _articlesState: MutableStateFlow<ArticlesState> = MutableStateFlow(ArticlesState())
 
     val articleState : StateFlow<ArticlesState> get() = _articlesState
+
+    init {
+        getArticles()
+    }
+
+    private fun getArticles() {
+        scope.launch {
+            delay(500)
+            _articlesState.emit(ArticlesState())
+        }
+    }
 }
